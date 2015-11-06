39479e166fa189840776ab5a5e74bfc290b2c030
Laci White <laciwhite48@gmail.com>
2015-11-05 17:07:30 -0800

    messing around with css

---
+++ script.js
@@ -0,0 +1,24 @@
+var x = 1 + 2;
+var y = x + x;
+
+var x = 1000;
+
+var brushedTeeth = confirm("did you brush your teeth today?");
+
+if (brushedTeeth){
+  alert("Yay you are not a neandrathal")
+} else {
+  alert("Grossssss!!!!")
+}
+
+var name = prompt("what is your name?")
+
+if (name === "shortcake") {
+  alert("I want strawberries")
+}
+else if (name === "short"){
+  alert("that's close to my name")
+}
+else {
+  alert("that is not my name")
+}
