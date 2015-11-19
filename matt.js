b9feb86d135ebc3b065a6d54faf4e7279e71db0e
Laci White <laciwhite48@gmail.com>
2015-11-19 00:06:21 -0800

    dom manipluations
    pain pain and more pain

---
+++ matt.js
@@ -0,0 +1,36 @@
+function convertKilometersToMiles (kilometer) {
+  var half = kilometer / 2;
+  var quarter = half / 4;
+  var mile = half + quarter;
+
+  return mile;
+}
+
+
+
+var x = 1;
+var y = 2;
+
+first(x, y);
+
+##
+
+var a = 25;
+var b = 75;
+
+function first(a, b) {
+  return a;
+}
+
+first(b, a) # first(75, 25)
+
+function hide(element){
+    element.setAttribute("style", "display: none;");
+}
+
+var myDiv = document.getElementByClass(".my-div");
+
+hide(myDiv);
+
+var mySecondDiv = document.getElementByClass(".my-second-div")
+hide(mySecondDiv);
