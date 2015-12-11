629521ed01d112c06a749c71cbf6555e77fbacdc
Laci White <laciwhite48@gmail.com>
2015-12-10 16:00:43 -0800

    stuff that might break things

--- clock/clock.js
+++ clock/clock.js
@@ -1,16 +1,16 @@
 
-// $(document).ready( function() {
+
+function addZero(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
 
   function displayTime() {
 
     var currentTime = new Date();
-
     var hours = currentTime.getHours();
-    addZero(hours);
     var minutes = currentTime.getMinutes();
-    addZero(minutes);
     var seconds = currentTime.getSeconds();
-    addZero(seconds);
 
     var clockDiv = document.getElementById("clock").innerHTML = hours + ":" + minutes + ":" + seconds;
 
@@ -19,8 +19,3 @@
   };
 
     displayTime();
-
-function addZero(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
